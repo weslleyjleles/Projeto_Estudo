348a94ff9fe411f403f4d27a70ab68c7581a2d8d
weslley <weslleyjleles@gmail.com>
2022-12-12 12:04:27 -0300

    adicao comment / like / controller

---
+++ src/main/java/br/com/ProjetoEstudo/Controller/UsuarioController.java
@@ -0,0 +1,5 @@
+package br.com.ProjetoEstudo.Controller;
+
+public class UsuarioController {
+
+}
---
+++ src/main/java/br/com/ProjetoEstudo/Model/UsuarioModel.java
@@ -0,0 +1,16 @@
+package br.com.ProjetoEstudo.Model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@Entity
+public class UsuarioModel {
+public String nome;
+public String email;
+public String senha;
+	
+	
+}
